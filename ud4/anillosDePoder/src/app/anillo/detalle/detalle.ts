e2552fe32dd1e49922a4b3bf315a6a792289b473
alion1992 <55086590+alion1992@users.noreply.github.com>
2026-01-12 12:48:07 +0100

    anillos
    subir  anillos de poder

--- ud4/anillosDePoder/src/app/anillo/detalle/detalle.ts
+++ ud4/anillosDePoder/src/app/anillo/detalle/detalle.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './detalle.html',
   styleUrl: './detalle.css',
 })
